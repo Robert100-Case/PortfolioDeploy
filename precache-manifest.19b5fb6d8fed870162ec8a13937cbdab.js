80e2f9cdf5473e693240cd9eccc7589aadf72aa8
Robert Rossman <brossman01@outlook.com>
2021-08-01 17:48:47 -0400

    Updates

---
+++ precache-manifest.19b5fb6d8fed870162ec8a13937cbdab.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae9b33084a176d6f177bcc6e87a7ebdf",
+    "url": "/PortfolioDeploy/index.html"
+  },
+  {
+    "revision": "4fc69d081564858b3560",
+    "url": "/PortfolioDeploy/static/css/main.1f6db562.chunk.css"
+  },
+  {
+    "revision": "e8510e50cd7d65205673",
+    "url": "/PortfolioDeploy/static/js/2.1d2e86b7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/PortfolioDeploy/static/js/2.1d2e86b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc69d081564858b3560",
+    "url": "/PortfolioDeploy/static/js/main.b985ad43.chunk.js"
+  },
+  {
+    "revision": "2f1be285285f1efae0f8",
+    "url": "/PortfolioDeploy/static/js/runtime-main.c10d73ef.js"
+  },
+  {
+    "revision": "402a471b5aad190a33b649f1368f5de1",
+    "url": "/PortfolioDeploy/static/media/02-hero-bg.402a471b.jpg"
+  },
+  {
+    "revision": "744105778413b0bbf30fc022b102d023",
+    "url": "/PortfolioDeploy/static/media/Budget.74410577.png"
+  },
+  {
+    "revision": "069d5be348ae3fc69dd3457c897fea9e",
+    "url": "/PortfolioDeploy/static/media/Pokemon2.069d5be3.png"
+  },
+  {
+    "revision": "16f43f81a0616b39087000e63f4a1784",
+    "url": "/PortfolioDeploy/static/media/Robert_Rossman_Resume.16f43f81.PDF"
+  },
+  {
+    "revision": "d993b41c719101f018761741c80e8760",
+    "url": "/PortfolioDeploy/static/media/TeamProfile.d993b41c.png"
+  },
+  {
+    "revision": "fbe4471474b0fe49d8341957e75830c1",
+    "url": "/PortfolioDeploy/static/media/dayplanner.fbe44714.jpg"
+  },
+  {
+    "revision": "e14f57d581f975b894fabfb208619110",
+    "url": "/PortfolioDeploy/static/media/picture of me.e14f57d5.jpg"
+  },
+  {
+    "revision": "98d561d3b7929e064fe4f37e632e2ccc",
+    "url": "/PortfolioDeploy/static/media/searchNews.98d561d3.png"
+  },
+  {
+    "revision": "6c4c82c78ec45c751de936616ff63c2c",
+    "url": "/PortfolioDeploy/static/media/weatherdashboard2.6c4c82c7.png"
+  }
+]);
